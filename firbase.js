259938af363ec8b3d98456b10c50d34c530a9185
BassemSiam <bassemsiam644@gmail.com>
2025-12-09 23:53:12 +0200

    fix fire

--- firbase.js
+++ firbase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics , logEvent } from "firebase/analytics";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
